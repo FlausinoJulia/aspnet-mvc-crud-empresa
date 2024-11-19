d3cc3d23c659850835d1bd2770b218ce3c8facee
Julia Flausino <julia.silva@narwalsistemas.com.br>
2024-11-19 16:15:12 -0300

    Alterando visual dos botões

--- CrudEmpresa/CrudEmpresa/Models/Empresa/Empresa.cs
+++ CrudEmpresa/CrudEmpresa/Models/Empresa/Empresa.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace CrudEmpresa.Models
 {
@@ -9,7 +10,9 @@ namespace CrudEmpresa.Models
         public string Nome { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
+        [DisplayName("País")]
         public string Pais { get; set; }
+        [DisplayName("Data cadastro")]
         public DateTime DataCadastro { get; set; }
     }
 }
--- CrudEmpresa/CrudEmpresa/ViewModel/Empresa/EmpresaDetailsViewModel.cs
+++ CrudEmpresa/CrudEmpresa/ViewModel/Empresa/EmpresaDetailsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -12,7 +13,9 @@ namespace CrudEmpresa.ViewModel.Empresa
         public string Nome { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
+        [DisplayName("País")]
         public string Pais { get; set; }
+        [DisplayName("Data cadastro")]
         public DateTime DataCadastro { get; set; }
     }
 }
